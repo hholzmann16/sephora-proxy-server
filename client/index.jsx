fe4e05b425c5d0d29d144da93be00ea7ea2a9569
Hannah Holzmann <hholzmann16@gmail.com>
2018-09-18 22:22:20 -0700

    Initial commit

---
+++ client/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Drew from "sephoranavbar/client/components/app.jsx";
+import Hannah from "sephora-items/client/src/components/ItemDescription.jsx";
+import Anthony from "fullstack-workshop/client/components/app.jsx";
+import Alex from "houselannister/client/component/App.jsx";
+
+ReactDOM.render(
+  <div>
+    <Drew />
+    <Hannah />
+    <Alex />
+    <Anthony />
+  </div>,
+  document.getElementById("root")
+);
---
+++ server/index.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var path = require("path");
+var axios = require("axios");
+var app = express();
+
+app.use(express.static(path.resolve(__dirname, "../client/dist")));
+// app.use(express.static(path.resolve(__dirname, "../deps")));
+
+app.get("/product", (req, res) => {
+  axios
+    .get("http://localhost:1337/product", req.params)
+    .then(d => res.send(d.data))
+    .catch(e => res.send(e));
+});
+
+app.get("/api", (req, res) => {
+  axios
+    .get("http://localhost:4000/api", { params: req.query })
+    .then(d => res.send(d.data))
+    .catch(e => res.send(e));
+});
+
+app.get("/api/mydb", (req, res) => {
+  axios
+    .get("http://localhost:3000/api/mydb", req.params)
+    .then(d => res.send(d.data))
+    .catch(e => res.send(e));
+});
+
+app.get("/review", (req, res) => {
+  axios
+    .get("http://localhost:5000/review", req.params)
+    .then(d => res.send(d.data))
+    .catch(e => res.send(e));
+});
+
+app.listen(3001, () => {
+  console.log("We in dis bitch");
+});
